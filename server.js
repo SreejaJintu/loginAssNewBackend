da94bba5bd3ee4392d3137ce650bdcb43ff9bf80
sreeja o s <sreejaosreenivasan@gmail.com>
2025-02-01 19:57:08 +0530

    bcrypt installed

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 
 const app = express();
 app.use(cors());
-app.use(cors({ origin: 'https://your-frontend-url.com', credentials: true }));
+app.use(cors({ origin: 'https://login-assinment-new.vercel.app/', credentials: true }));
 
 app.use(express.json());
 
@@ -14,4 +14,5 @@ connectDB();
 
 app.use('/api/auth', authRoutes);
 
-app.listen(5000, () => console.log('Server running on port 5000'));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
