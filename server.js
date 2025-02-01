ac9bb8b78ae28874be95df728579ef5bcbff1e02
sreeja o s <sreejaosreenivasan@gmail.com>
2025-02-01 21:54:24 +0530

    cors

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 
 const app = express();
 app.use(cors());
-app.use(cors({ origin: 'https://login-assinment-new.vercel.app/', credentials: true }));
+app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
 
 app.use(express.json());
 
