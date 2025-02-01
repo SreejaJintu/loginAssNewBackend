6860e6841ffe2f6714a78a3cee1f006bb3bb328d
sreeja o s <sreejaosreenivasan@gmail.com>
2025-02-01 22:01:08 +0530

    cors2

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
-const User = require('../models/user.js');
+const User = require('../models/User.js');
 
 const router = express.Router();
 const JWT_SECRET = process.env.JWT_SECRET;
