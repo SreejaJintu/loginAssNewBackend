669d118bbd070fb60b002d3059428232a7171053
sreeja o s <sreejaosreenivasan@gmail.com>
2025-02-02 22:57:19 +0530

    cors

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 
 const app = express();
 app.use(cors());
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors({ origin: ['http://localhost:3000','https://loginassinmentnew.onrender.com'], credentials: true }));
 
 app.use(express.json());
 
